6b6a75cce77443b7fc424404257adc1437fe6cb0
Malshii <kulasinghe1malshi@gmail.com>
2025-09-03 12:12:52 +0530

    Fix build issues - remove improper Html imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true, // Required for static export
     domains: ["kfzgnrxnyxiuankcmyth.supabase.co"],
   },
 };
